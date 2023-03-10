fd59edbd00acf28709eb9c9883b8db989590aef7
sarahxbkelly <sarah.kelly@dentsu.com>
2023-03-10 22:57:03 +0000

    cleaned code and had array output

--- python-survey-app/main.py
+++ python-survey-app/main.py
@@ -2,26 +2,27 @@ import pandas as pd
 
 from csv_processor import read_csv
 import csv
-def write_csv(row, file_path):
-    with open(file_path, 'w') as f:
-        writer = csv.writer(f, delimiter=',')
-        writer.writerow(row)
-def append_csv(row, file_path):
-    with open(file_path, 'a') as f:
-        writer = csv.writer(f, delimiter=',')
-        writer.writerow(row)
 
 file_path = './results.csv'# Read the fileprint(read_csv(file_path))
 
 results = (read_csv(file_path))
 
-df = pd.read_csv('./results.csv')
+duplicates = set()
 
-df.drop_duplicates(subset=['user_id'], inplace=True)
+cleanresults = []
 
-df.dropna(inplace=True)
+for rec in results:
+    id = rec[0]
+    if id not in duplicates:
+        duplicates.add(id)
+        cleanresults.append(rec)
 
-print(df)
+print(cleanresults)
+
+
+
+
+#print(results)
 
 
 
