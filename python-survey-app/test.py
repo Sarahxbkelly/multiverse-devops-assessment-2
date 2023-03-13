60535268e00d70d584dd42fba0708696e8580e98
sarahxbkelly <sarah.kelly@dentsu.com>
2023-03-13 10:39:15 +0000

    new test for ticket one

--- python-survey-app/test.py
+++ python-survey-app/test.py
@@ -6,12 +6,22 @@ def test_input_is_list():
     expected_output = list
 
     #Act
-    output = (read_csv(filename))
+    output = read_csv(filename)
 
     #Assert
     assert type(output) == expected_output
 
+def test_input_is_correct():
+    #Arrange
+    filename = "./results.csv"
+    expected_output = ['user_id', 'first_name', 'last_name', 'answer_1', 'answer_2', 'answer_3']
 
+    #Act
+    output = read_csv(filename)
+    actual_output = output[0]
+
+    #Assert
+    assert actual_output == expected_output
 
 
 
