8dbfb94052713f013ff36cf671d73d93b2b69750
sarahxbkelly <sarah.kelly@dentsu.com>
2023-03-13 17:08:49 +0000

    init commit of application script

---
+++ python-survey-app/main.py
@@ -0,0 +1,41 @@
+import csv
+from csv_processor import read_csv # ticket 1
+from remove_duplicate import remove_duplicates #ticket 2
+from remove_empty_record import remove_empty_records #ticket 3
+from capitalise import capitalise #ticket 4
+from validate_field import validate_field #ticket 5
+
+
+file_path = './results.csv'
+
+results = (read_csv(file_path))
+
+cleanresults = validate_field(remove_duplicates(remove_empty_records(capitalise(results))))
+
+#print(cleanresults)
+
+with open('clean_results.csv', mode='w', newline='') as clean_results:
+    writer = csv.writer(clean_results, delimiter=',')
+    writer.writerows(cleanresults) #ticket 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
