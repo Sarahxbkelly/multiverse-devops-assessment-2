022ee2fe649537ef9c36d6ceb8582762561d3af8
Sarahxbkelly <93191051+Sarahxbkelly@users.noreply.github.com>
2023-03-15 22:25:28 +0000

    Delete csv_processor.py
    Added the folder file instead

--- python-survey-app/csv_processor.py
+++
@@ -1,9 +0,0 @@
-import csv
-def read_csv(file_path):
-    rows = []
-    with open(file_path, 'r') as f:
-        reader = csv.reader(f, delimiter=',')
-        for row in reader:
-            rows.append(row)
-    return rows
-
