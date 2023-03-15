3340c8310a2ce871ae05cdefb421affa1252da3b
sarahxbkelly <sarah.kelly@dentsu.com>
2023-03-15 22:16:53 +0000

    ticket 7

---
+++ python-survey-app/output.py
@@ -0,0 +1,16 @@
+from csv_processor import read_csv # ticket 7
+import csv
+
+file_path = './clean_results.csv'# Read the fileprint(read_csv(file_path))
+
+clean_results = (read_csv(file_path))
+
+for row in clean_results:
+    print (row)
+    
+
+
+#print(clean_results)
+
+  
+
